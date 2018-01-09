40de9a4d42cbba72e215f2d8112023d617b8bb58
Unknown <mr.kachanov@gmail.com>
2018-01-09 17:15:05 +0200

    resolved Speaker_upgrade

---
+++ speaker_upgrade.js
@@ -0,0 +1,26 @@
+class Speaker {
+
+    constructor(name, verb){
+        this.name = name;
+        this.verb = verb || "says";
+    }
+
+    speak(text) {
+        console.log(this.name + " " + this.verb + " '" + text + "'")
+    }
+
+}
+
+class Shouter extends Speaker {
+
+    constructor(name){
+        super(name, "shouts")
+    }
+
+    speak(text){
+        super.speak(text.toUpperCase());
+    }
+}
+
+
+new Shouter("Dr. Loudmouth").speak("hello there");
