935512999bdf3700658fd437c16352e73c612697
Unknown <mr.kachanov@gmail.com>
2018-01-10 10:56:01 +0200

    resolved configurable_property

---
+++ Object literals/Configurable property/configurable_property.js
@@ -0,0 +1,12 @@
+function startNode(type, value, options) {
+    return{
+        type,
+        value,
+        [options.sourceProperty]: options.sourceValue
+    }
+}
+
+console.log(startNode("Identifier", "foo", {
+    sourceProperty: "src",
+    sourceValue: "bar.js"
+}));
