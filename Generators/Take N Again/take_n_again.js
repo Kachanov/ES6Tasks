f5fd723ad1e7dfd35b726fa4e2aadfa0e9b596c7
Unknown <mr.kachanov@gmail.com>
2018-01-15 16:59:00 +0200

    refactored

--- Generators/Take N Again/take_n_again.js
+++ Generators/Take N Again/take_n_again.js
@@ -1,10 +1,53 @@
 function* integers() {
-    for (let i = 0;; i++) yield i
+    for (let i = 0; ; i++) {
+        yield i;
+    }
 }
+
 function* take(n, iter) {
     for (let i = 0; i < n; i++) {
         yield iter.next().value;
     }
 }
+
+for (let elt of take(5, integers())) {
+    console.log(elt);
+}
+
+
+/*function integers() {
+    let i = 0;
+    return {
+        next() {
+            return {
+                value: i++
+            }
+        },
+        [Symbol.iterator]() { return this }}
+}
+
+function take(n, iter) {
+    var index = 0;
+    return {
+        next() {
+            if (index < n) {
+                index++;
+                return {
+                    done: false,
+                    value: iter.next().value
+                }
+            }else {
+                return {
+                    done: true
+                }
+            }
+
+        },
+        [Symbol.iterator]() {
+            return this
+        }
+    }
+}
+
 for (let elt of take(3, integers()))
-    console.log(elt)
+    console.log(elt)*/
